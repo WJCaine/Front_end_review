3499a503695926158c6457dadd60168bd405a7e7
Your Name <Billy.caine@gmail.com>
2019-11-01 14:24:27 +0000

    slightly improved time thing

--- nc-news/src/utils.js
+++ nc-news/src/utils.js
@@ -2,7 +2,7 @@ export const repetitionAvoider = (timeUnit, timeLength, seconds) => {
   if (seconds / timeLength > 1) {
     let timeUnitAmount = Math.floor(seconds / timeLength);
     seconds = seconds % timeLength;
-    if (timeUnit === 1) return `1 ${timeUnit} `;
+    if (timeUnitAmount === 1) return `1 ${timeUnit} `;
     else {
       return `${timeUnitAmount} ${timeUnit}s `;
     }
@@ -11,18 +11,17 @@ export const repetitionAvoider = (timeUnit, timeLength, seconds) => {
 
 export const secondsToTimeString2 = seconds => {
   let readableString = "";
+  let counter = 0;
+  if (seconds >= 31536000) counter++;
+  if (seconds >= 86400) counter++;
+
   readableString += repetitionAvoider("year", 31536000, seconds);
   seconds = seconds % 31536000;
   readableString += repetitionAvoider("day", 86400, seconds);
   seconds = seconds % 86400;
-  readableString += repetitionAvoider("hour", 3600, seconds);
+  if (counter < 2) readableString += repetitionAvoider("hour", 3600, seconds);
   seconds = seconds % 3600;
-  readableString += repetitionAvoider("minute", 60, seconds);
-  seconds = seconds % 60;
-  // if (seconds === 1) {
-  //   readableString += "1 second";
-  // } else {
-  //   readableString += `${seconds} seconds`;
-  // }
+  if ((counter = 0)) readableString += repetitionAvoider("minute", 60, seconds);
+
   return readableString;
 };
